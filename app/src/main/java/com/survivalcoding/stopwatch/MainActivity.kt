16ca294b1c754e641f07a9bbab38a07b3a56b697
Junsuk Oh <junsuk.oh@survivalcoding.com>
2022-12-27 12:01:09 +0900

    by lazy 활용

--- app/src/main/java/com/survivalcoding/stopwatch/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/stopwatch/MainActivity.kt
@@ -9,13 +9,14 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.survivalcoding.stopwatch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
