15f3486eabdd688992a6366808b78be7c22765ed
Junsuk Oh <junsuk.oh@survivalcoding.com>
2022-12-27 11:49:05 +0900

    ViewModel + LiveData 활용법

--- app/src/main/java/com/survivalcoding/stopwatch/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/stopwatch/MainActivity.kt
@@ -3,25 +3,26 @@ package com.survivalcoding.stopwatch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-
         val addButton = findViewById<FloatingActionButton>(R.id.add_button)
         val countTextView : TextView = findViewById(R.id.count_text_view)
 
-        countTextView.text = "${viewModel.count}"
+        viewModel.countLiveData.observe(this) { count ->
+            countTextView.text = "$count"
+        }
 
         addButton.setOnClickListener {
-            viewModel.count++
-            countTextView.text = "${viewModel.count}"
+            viewModel.increase()
         }
     }
 }
--- app/src/main/java/com/survivalcoding/stopwatch/MainViewModel.kt
+++ app/src/main/java/com/survivalcoding/stopwatch/MainViewModel.kt
@@ -1,7 +1,20 @@
 package com.survivalcoding.stopwatch
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    var count = 0
+    private var count = 0
+
+    val countLiveData = MutableLiveData(count)
+
+    fun increase() {
+        count++
+        countLiveData.value = count
+    }
+
+    fun decrease() {
+        count--
+        countLiveData.value = count
+    }
 }
