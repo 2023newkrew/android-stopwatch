eb41a894bef7c5c55d47e8b14a2333ee0819ef44
Junsuk Oh <junsuk.oh@survivalcoding.com>
2022-12-27 11:51:17 +0900

    viewBinding 적용

--- app/src/main/java/com/survivalcoding/stopwatch/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/stopwatch/MainActivity.kt
@@ -6,22 +6,24 @@ import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.survivalcoding.stopwatch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val addButton = findViewById<FloatingActionButton>(R.id.add_button)
-        val countTextView : TextView = findViewById(R.id.count_text_view)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         viewModel.countLiveData.observe(this) { count ->
-            countTextView.text = "$count"
+            binding.countTextView.text = "$count"
         }
 
-        addButton.setOnClickListener {
+        binding.addButton.setOnClickListener {
             viewModel.increase()
         }
     }
