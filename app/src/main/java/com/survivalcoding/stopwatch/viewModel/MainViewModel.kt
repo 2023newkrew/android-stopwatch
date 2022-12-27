76be90a149232acda48fc1cf7c8cf2e805f24554
comseung18 <40685565+comseung18@users.noreply.github.com>
2022-12-27 20:01:22 +0900

    메인뷰모델 플레이/스탑 함수 추가

--- app/src/main/java/com/survivalcoding/stopwatch/viewModel/MainViewModel.kt
+++ app/src/main/java/com/survivalcoding/stopwatch/viewModel/MainViewModel.kt
@@ -1,6 +1,28 @@
 package com.survivalcoding.stopwatch.viewModel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.util.*
+import kotlin.concurrent.timer
 
 class MainViewModel : ViewModel() {
+    private var milSec:Long = 0
+
+    companion object{
+        const val TIMER_PERIOD = 10L
+    }
+    private var timer: Timer? = null
+
+    val milSecLiveData = MutableLiveData<Long>(milSec)
+
+    fun timerPlay(){
+        timer = timer(period = TIMER_PERIOD){
+            milSec += TIMER_PERIOD
+            milSecLiveData.postValue(milSec)
+        }
+    }
+
+    fun timerStop(){
+        timer?.cancel()
+    }
 }
