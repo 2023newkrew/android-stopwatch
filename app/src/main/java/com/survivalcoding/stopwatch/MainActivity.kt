c0eb9846bf423e581038c45fdc179b6c56b1994e
bard.kim <bard.kim@kakaocorp.com>
2022-12-28 17:01:15 +0900

    feat : fragment 추가, bottom nav 추가

--- app/src/main/java/com/survivalcoding/stopwatch/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/stopwatch/MainActivity.kt
@@ -17,81 +17,11 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
     private val viewModel: MainViewModel by viewModels()
-    private val df00 = DecimalFormat("00")
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val view = binding.root
         setContentView(view)
-        val blinkAnim = AnimationUtils.loadAnimation(this, R.anim.blink_animation)
-
-        viewModel.liveStateData.observe(this) { state ->
-            if (state.hour > 0) {
-                binding.hourText.text = "${state.hour}"
-                binding.hourColon.text = ":"
-            } else {
-                binding.hourText.text = ""
-                binding.hourColon.text = ""
-            }
-            if (state.minute > 0) {
-                binding.minuteText.text = "${state.minute}"
-                binding.minuteColon.text = ":"
-            } else {
-                binding.minuteText.text = ""
-                binding.minuteColon.text = ""
-            }
-            binding.secondText.text = df00.format(state.sec)
-            binding.millisecondText.text = df00.format(state.milliSec)
-        }
-
-        viewModel.liveProgressPercent.observe(this) { percent ->
-            binding.progressiveTimerButton.progress = percent
-        }
-
-        binding.startPauseButton.setOnClickListener {
-            binding.resetButton.isVisible = true
-            if (viewModel.isPaused) {
-                stopAnimation(blinkAnim)
-                binding.startPauseButton.setImageResource(R.drawable.ic_baseline_pause_24)
-                viewModel.start()
-                binding.recordButton.isVisible = true
-            } else {
-                startAnimation(blinkAnim)
-                // TODO Progressive bar 추가
-                binding.startPauseButton.setImageResource(R.drawable.ic_baseline_play_arrow_24)
-                viewModel.pause()
-                binding.recordButton.isVisible = false
-            }
-            if (binding.startPauseMotion?.progress == 0.0F) {
-                binding.startPauseMotion?.transitionToEnd()
-            } else {
-                binding.startPauseMotion?.transitionToStart()
-            }
-
-        }
-        binding.resetButton.setOnClickListener {
-            stopAnimation(blinkAnim)
-            binding.startPauseButton.setImageResource(R.drawable.ic_baseline_play_arrow_24)
-            binding.startPauseMotion?.transitionToStart()
-            viewModel.reset()
-            binding.resetButton.visibility = View.INVISIBLE
-            binding.recordButton.visibility = View.INVISIBLE
-        }
-        binding.recordButton.setOnClickListener {
-            viewModel.lapTime()
-        }
     }
-
-    private fun startAnimation(anim: Animation) {
-        binding.timeLayout.startAnimation(anim)
-        binding.millisecondText.startAnimation(anim)
-    }
-
-    private fun stopAnimation(anim: Animation) {
-        binding.timeLayout.clearAnimation()
-        binding.millisecondText.clearAnimation()
-    }
-
 
 }
---
+++ app/src/main/java/com/survivalcoding/stopwatch/fragment/stopWatchFragment.kt
@@ -0,0 +1,105 @@
+package com.survivalcoding.stopwatch.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.core.view.isVisible
+import androidx.fragment.app.activityViewModels
+import com.survivalcoding.stopwatch.MainViewModel
+import com.survivalcoding.stopwatch.R
+import com.survivalcoding.stopwatch.databinding.FragmentStopWatchBinding
+import java.text.DecimalFormat
+
+class stopWatchFragment : Fragment() {
+
+    private var _binding: FragmentStopWatchBinding? = null
+    private val binding get() = _binding!!
+    private val df00 = DecimalFormat("00")
+    private val viewModel: MainViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentStopWatchBinding.inflate(inflater, container, false)
+        val view = binding.root
+        val blinkAnim = AnimationUtils.loadAnimation(context, R.anim.blink_animation)
+
+        viewModel.liveStateData.observe(viewLifecycleOwner) { state ->
+            if (state.hour > 0) {
+                binding.hourText.text = "${state.hour}"
+                binding.hourColon.text = ":"
+            } else {
+                binding.hourText.text = ""
+                binding.hourColon.text = ""
+            }
+            if (state.minute > 0) {
+                binding.minuteText.text = "${state.minute}"
+                binding.minuteColon.text = ":"
+            } else {
+                binding.minuteText.text = ""
+                binding.minuteColon.text = ""
+            }
+            binding.secondText.text = df00.format(state.sec)
+            binding.millisecondText.text = df00.format(state.milliSec)
+        }
+
+        viewModel.liveProgressPercent.observe(viewLifecycleOwner) { percent ->
+            binding.progressiveTimerButton.progress = percent
+        }
+
+        binding.startPauseButton.setOnClickListener {
+            binding.resetButton.isVisible = true
+            if (viewModel.isPaused) {
+                stopAnimation()
+                binding.startPauseButton.setImageResource(R.drawable.ic_baseline_pause_24)
+                viewModel.start()
+                binding.recordButton.isVisible = true
+            } else {
+                startAnimation(blinkAnim)
+                // TODO Progressive bar 추가
+                binding.startPauseButton.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+                viewModel.pause()
+                binding.recordButton.isVisible = false
+            }
+            if (binding.startPauseMotion?.progress == 0.0F) {
+                binding.startPauseMotion?.transitionToEnd()
+            } else {
+                binding.startPauseMotion?.transitionToStart()
+            }
+
+        }
+        binding.resetButton.setOnClickListener {
+            stopAnimation()
+            binding.startPauseButton.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+            binding.startPauseMotion?.transitionToStart()
+            viewModel.reset()
+            binding.resetButton.visibility = View.INVISIBLE
+            binding.recordButton.visibility = View.INVISIBLE
+        }
+        binding.recordButton.setOnClickListener {
+            viewModel.lapTime()
+        }
+        return view
+    }
+
+    private fun startAnimation(anim: Animation) {
+        binding.timeLayout.startAnimation(anim)
+        binding.millisecondText.startAnimation(anim)
+    }
+
+    private fun stopAnimation() {
+        binding.timeLayout.clearAnimation()
+        binding.millisecondText.clearAnimation()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
