d1366d39904c95fc5f30571818cde47f7085a35f
comseung18 <40685565+comseung18@users.noreply.github.com>
2022-12-29 17:23:46 +0900

    feat: actionbar 에서 toolbar 를 사용하도록 함

--- app/src/main/java/com/survivalcoding/stopwatch/fragment/StopWatchFragment.kt
+++ app/src/main/java/com/survivalcoding/stopwatch/fragment/StopWatchFragment.kt
@@ -1,7 +1,6 @@
 package com.survivalcoding.stopwatch.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +8,7 @@ import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.survivalcoding.stopwatch.R
 import com.survivalcoding.stopwatch.databinding.FragmentStopWatchBinding
