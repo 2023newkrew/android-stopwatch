db5760998ffff056e972e7dd8cfdbd91120d4073
comseung18 <40685565+comseung18@users.noreply.github.com>
2022-12-29 20:00:52 +0900

    feat: MainViewModel 에서 repository 사용하도록 하고 DI 하는 팩토리 추가

--- app/src/main/java/com/survivalcoding/stopwatch/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/survivalcoding/stopwatch/viewmodel/MainViewModel.kt
@@ -1,11 +1,13 @@
 package com.survivalcoding.stopwatch.viewmodel
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.survivalcoding.stopwatch.entity.LabTime
+import com.survivalcoding.stopwatch.repository.LabTimeRepository
+import kotlinx.coroutines.launch
 import java.util.*
 import kotlin.concurrent.timer
 
-class MainViewModel : ViewModel() {
+class MainViewModel(private val repository: LabTimeRepository) : ViewModel() {
     private var milSec: Long = 0L
 
 
@@ -19,11 +21,26 @@ class MainViewModel : ViewModel() {
     var isPlaying: Boolean = false
     var isPlayedOneMore: Boolean = false
 
+    val allLabTimes: LiveData<List<LabTime>> = repository.allLabTimes.asLiveData()
+
+    private fun insert(labTime: LabTime) = viewModelScope.launch {
+        repository.insert(labTime)
+    }
+
+    private fun deleteAll() = viewModelScope.launch {
+        repository.deleteAll()
+    }
+
     fun init() {
         timerStop()
         milSec = 0
         milSecLiveData.postValue(milSec)
         isPlayedOneMore = false
+        deleteAll()
+    }
+
+    fun record() {
+        insert(LabTime(time = milSec))
     }
 
 
@@ -48,5 +65,15 @@ class MainViewModel : ViewModel() {
     override fun onCleared() {
         super.onCleared()
         timerStop()
+    }
+}
+
+class MainViewModelFactory(private val repository: LabTimeRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return MainViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
